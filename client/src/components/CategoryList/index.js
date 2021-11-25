b650eb55299ea5056389dd938e68d545722c49b3
Benicio Lopez <benicio.lopez@gmail.com>
2021-11-25 08:34:55 -0600

    got the photos to render in the CategoryList component

---
+++ client/src/components/CategoryList/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CategoryList = () => {
+    const categoryCategory = 'not sure what I need yet a la verga tilin';
+
+    return (
+        <div>
+            <h2>Here is where the picture is below:</h2>
+            <img
+                alt='Chainsaw'
+                src={require('../../images/Lawn&Garden.png')}
+            />
+        </div>
+    )
+
+
+};
+
+export default CategoryList
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,13 @@ import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
+import CategoryList from '../components/CategoryList';
 
 const Home = () => {
   return (
     <div className="container">
       <CategoryMenu />
-      <ProductList />
+      <CategoryList />
       <Cart />
     </div>
   );
