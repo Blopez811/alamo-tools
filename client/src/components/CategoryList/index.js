ce9b47744042c5b30674345125f7d9d8ee77fdf2
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-11-26 15:33:06 -0600

    card header class added to RENTAL EQUIPMENT, has its own card now, consult later for color desicions

--- client/src/components/CategoryList/index.js
+++ client/src/components/CategoryList/index.js
@@ -5,10 +5,12 @@ const CategoryList = () => {
     const categoryCategory = 'not sure what I need yet a la verga tilin';
 
     return (
-        <div class='home-text'>
+        <div>
+          <div class='home-text'>
             <h1 class='card-header'>RENTAL EQUIPMENT</h1>
             <div>
             <h3>Select a category below to view rental equipment information or to reserve equipment for your jobsite.</h3>
+            </div>
             <div class='row'>
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src='../../images/Lawn&Garden.png' />
@@ -38,6 +40,17 @@ const CategoryList = () => {
                 <Card.Img variant="top" src="../../images/Concrete & Masonry.png" />
                 <Card.Body>
                     <Card.Title>Concrete & Masonry</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Check Availability</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="../../images/Concrete & Masonry.png" />
+                <Card.Body>
+                    <Card.Title>Murder weapons</Card.Title>
                     <Card.Text>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
