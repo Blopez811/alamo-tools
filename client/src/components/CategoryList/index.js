df72f6b8b9a5917d648bc71ff70ccc2b82963a94
Benicio Lopez <benicio.lopez@gmail.com>
2021-11-28 21:17:47 -0600

    all buttons on category list now render single category page

--- client/src/components/CategoryList/index.js
+++ client/src/components/CategoryList/index.js
@@ -40,7 +40,9 @@ const CategoryList = () => {
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
                     </Card.Text>
+                    <Link to={`/single-category`}>
                     <Button variant="primary" onClick={displayCategory}>Check Availability</Button>
+                    </Link>
                 </Card.Body>
             </Card>
 
@@ -52,7 +54,9 @@ const CategoryList = () => {
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
                     </Card.Text>
+                    <Link to={`/single-category`}>
                     <Button variant="primary" onClick={displayCategory}>Check Availability</Button>
+                    </Link>
                 </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }}>
@@ -63,7 +67,9 @@ const CategoryList = () => {
                         Here you'll find rope, duct-tape, gloves, knives, and all the accoutrements of the 
                         Dark Arts
                     </Card.Text>
+                    <Link to={`/single-category`}>
                     <Button variant="primary" onClick={displayCategory}>Check Availability</Button>
+                    </Link>
                 </Card.Body>
             </Card>
             </div>
