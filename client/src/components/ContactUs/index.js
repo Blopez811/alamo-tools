7c50a7f285f13d94922990b656962a94b23fb721
Benicio Lopez <benicio.lopez@gmail.com>
2021-11-29 09:35:45 -0600

    set up ContactUs component

---
+++ client/src/components/ContactUs/index.js
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
+const ContactUs = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_USER_ID')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+  
+    return (
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="name" />
+        <label>Email</label>
+        <input type="email" name="email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    );
+};
+
+export default ContactUs;
